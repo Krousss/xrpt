b18a6449b4ea0e82d797092ba5e00c6dde4d8227
DESKTOP-DQQAMG9\Sparta117 <1159637378@qq.com>
2020-07-13 14:49:58 +0800

    国科Web 2004班 B组 Springboot考核项目

--- src/main/java/com/xrpt/demo/controller/JumpController.java
+++ src/main/java/com/xrpt/demo/controller/JumpController.java
@@ -1,5 +1,6 @@
 package com.xrpt.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -7,10 +8,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @date 2020/7/13
  * @DESC:这个Controller用来控制A标签的跳转。
  */
+@Controller
 public class JumpController {
 
     @RequestMapping("/toPosterCenter")
     public String toPosterCenter(){
         return "Poster/PosterCenter";
+    }
+
+    @RequestMapping("/toUserCenter")
+    public String toUserCenter(){
+        return "User/UserCenter";
     }
 }
